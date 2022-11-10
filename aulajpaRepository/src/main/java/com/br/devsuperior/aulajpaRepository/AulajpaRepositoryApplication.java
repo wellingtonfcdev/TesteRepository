ca93342bc96045fddbf8bebaece63b9cb83d36cb
Wellington Feijó <wellingtonfc95@gmail.com>
2022-11-10 00:38:44 -0300

    Iniciando os trabalhos

---
+++ aulajpaRepository/src/main/java/com/br/devsuperior/aulajpaRepository/AulajpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.br.devsuperior.aulajpaRepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulajpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulajpaRepositoryApplication.class, args);
+	}
+
+}
---
+++ aulajpaRepository/src/test/java/com/br/devsuperior/aulajpaRepository/AulajpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.devsuperior.aulajpaRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulajpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
